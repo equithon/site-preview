5b5acbf87c4918ddc652dd6043a3a4db75c29ea6
Alex Xie <alexieyizhe@gmail.com>
2018-08-16 01:06:37 -0400

    Finish styling for mailing list signup

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Particles from 'react-particles-js';
 import { isMobile } from 'react-device-detect';
+import MtSvgLines from 'react-mt-svg-lines';
 import {
   mediaSize,
   particleConfig
@@ -93,6 +94,7 @@ const ActionContainer = styled.div`
                        "exec-team mailing-list";
 
   ${mediaSize.phone`
+    width: 95%;
     justify-self: center;
     grid-template-columns: auto;
     grid-template-rows: 2em 2em 2em;
@@ -155,6 +157,73 @@ const ActionButton = styled.div`
   `};
 `;
 
+const ActionInput = styled.div`
+  margin-top: 1.5em;
+  height: 2.5em;
+  border-radius: 20px;
+  cursor: pointer;
+  background-color: #fff;
+	border-bottom: 2px solid #909090;
+	text-shadow: 0px -2px #fff;
+  text-align: center;
+  line-height: 2.5em;
+  position: relative;
+  overflow-y: hidden;
+
+  & textarea:focus, input:focus{
+    outline: none;
+  }
+
+  ${mediaSize.phone`
+    height: 8vw;
+    border-radius: 10px;
+    line-height: 8vw;
+    font-size: 3vw;
+  `};
+`;
+
+const ActionInputOverlay = styled.div`
+  background-color: rgba(255, 255, 255, 0);
+  color: ${props => props.color};
+  white-space: nowrap;
+  transition: transform 500ms;
+  width: ${props => props.width};
+  height: 2.5em;
+  position: absolute;
+  top: 0;
+  left: 5%;
+  transform: ${props => props.focused ? css`translateY(-2.5em)` : css`translateY(0)`}
+`;
+
+const ClickButton = styled.img`
+  max-width: 1em;
+  max-height: 1em;
+  float: right;
+  position: relative;
+  right: 0.5em;
+  top: 0.75em;
+  opacity: 0.4;
+`;
+
+const ActionInputBox = styled.input`
+  width: 70%;
+  height: 2.5em;
+  background: transparent;
+  border: none;
+  position: absolute;
+  left: 7%;
+  font-size: 1em;
+`;
+
+const ToastBox = styled.div`
+  font-size: 1rem;
+  margin-top: 10px;
+  color: ${props => props.fontColor || 'white'};
+  transition: all 500ms;
+  opacity: ${props => props.show ? '1': 0 };
+  transform: ${props => props.show ? css`translateY(0)` : css`translateY(1rem)`}
+`;
+
 const SocialContainer = styled.div`
   grid-area: social;
   font-size: 3vmin;
@@ -171,6 +240,7 @@ const SocialContainer = styled.div`
       opacity: 1;
     }
   }
+
 `;
 
 const Copyright = styled.div`
@@ -186,10 +256,40 @@ const Copyright = styled.div`
 class IndexPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      inputFocused: false,
+      curInput: '',
+      lastInputValid: null,
+    };
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    const userEmail = e.target.children && e.target.children[0].value;
+    if( /(.+)@(.+){2,}\.(.+){2,}/.test(userEmail) ){ // valid email
+      console.log(userEmail + " is valid");
+      this.setState({lastInputValid: true})
+    } else { // invalid email
+      console.log(userEmail + " is invalid");
+      this.setState({lastInputValid: false})
+    }
+
   }
 
   render() {
+    let imgSrc = '/submit.svg';
+    let toastMsg = null;
+    let toastColor = null;
+    if(this.state.lastInputValid === false) {
+      imgSrc = '/fail.svg';
+      toastMsg = 'Your email address seems funky. Try again.';
+      toastColor = '#fa5050';
+    }
+    if(this.state.lastInputValid === true){
+      imgSrc = '/success.svg';
+      toastMsg = 'You\'re set! Keep an eye on your inbox.';
+      toastColor = '#37d634';
+    }
     return (
       <div>
         {/*}<Particles params={particleConfig} style={ParticlesStyle} />*/}
@@ -208,9 +308,21 @@ class IndexPage extends React.Component {
             </div>
             <div style={{gridArea: 'mailing-list', fontSize: '2vmin'}}>
               { isMobile ? null : <span>Interested in participating? Be the first to receive updates by signing up. <br/></span> }
-              <ActionButton>
-                { isMobile ? "Sign Up For Updates" : "Keep Me Posted" }
-              </ActionButton>
+              <ActionInput onClick={() => this.setState({ inputFocused: true })} tabIndex="0" onBlur={() => this.setState({ inputFocused: false })} lastSubmitted={this.state.lastInputValid}>
+                <ActionInputOverlay focused={this.state.inputFocused} color="#a934f7" width="90%">
+                  { !this.state.curInput ? (isMobile ? "Sign Up For Updates" : "Keep Me Posted") : null }
+                </ActionInputOverlay>
+                <ActionInputOverlay focused={!this.state.inputFocused} color="rgba(142, 142, 142, 0.3)" width="90%">
+                  { !this.state.curInput ? (isMobile ? "Email" : "Your Email") : null }
+                  <ClickButton src={imgSrc} />
+                </ActionInputOverlay>
+                <form name="contact" method="POST" onSubmit={(e) => this.handleSubmit(e)} netlify>
+                  <ActionInputBox type="email" name="userEmail" onChange={(evt) => { this.setState({curInput: evt.target.value, lastInputValid: null}) }} />
+                </form>
+              </ActionInput>
+              <ToastBox fontColor={toastColor} show={toastMsg !== null}>
+                {toastMsg}
+              </ToastBox>
             </div>
           </ActionContainer>
           <SocialContainer>
